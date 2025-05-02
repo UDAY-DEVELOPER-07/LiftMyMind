dd9c0409bf55555bcbc32b85b878813cc34e6cd5
UDAY-DEVELOPER-07 <udaysinghno2005@gmail.com>
2025-05-02 23:31:15 +0530

    Working on the first page

--- LiftMyMind/src/App.tsx
+++ LiftMyMind/src/App.tsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function App() {
   return (
     <div className="min-h-screen w-full bg-black/[0.96] antialiased relative overflow-hidden">
-      <div className="relative z-[3] pt-40 pb-20 px-4">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 mb-8">
-            Lift My Mind
+      <div className="relative z-[3] lex justify-items-center justify-center items-center pt-40 pb-20 px-4">
+        <div className="max-w-7xl flex justify-items-center justify-center items-center  mx-auto">
+        <h1 className="text-4xl md:text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 mb-8">
+        Mental Health Matters: A Guide to Overcoming Depression and Anxiety
           </h1>
           <p className="text-xl text-neutral-300 max-w-3xl mx-auto">
             Welcome to Lift My Mind, a mental health app designed to help you
